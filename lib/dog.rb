3d8b3337e1878792ee0d745ec6d34bb91b8ca289
wendy <mizukilee@gmail.com>
2023-01-08 18:26:32 -0800

    commit tree 1bc9d5262056da0e08a9c5e2cd682126ae8bcf20

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    # Class Body 
+
+    # Instance Method Definition 
+    def bark 
+        puts 'Woof!'
+    end 
+
+    def sit 
+        puts 'The Dog is sitting'
+    end 
+end 
+
+fido = Dog.new
+fido.bark 
+
+snoopy = Dog.new 
+
+
